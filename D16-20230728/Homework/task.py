c789be947b70a3c6310b1232b5514172c12acc45
sangee-21 <sangeethakesavan76@gmail.com>
2023-08-08 14:12:58 +0530

    modification

--- D16-20230728/Homework/task.py
+++ D16-20230728/Homework/task.py
@@ -23,3 +23,31 @@ def find_sum(num):
     return pairs
 final=find_sum(list)
 print(final)
+
+def count(word):
+        sum=0
+        for char in word:
+            vowal="aeiouAEIOU"
+            if char in vowal:
+                sum=sum+1
+        return sum
+                
+
+                
+def max_vowal(line):
+    words=[]
+    
+    max_vowal_count=0
+    max_vowal_word=""
+    words=line.split()
+    
+    for word in words:
+        vowal_count=count(word)
+        if vowal_count>max_vowal_count:
+            max_vowal_count=vowal_count
+            max_vowal_word=word
+    print(max_vowal_word)
+    print(max_vowal_count)
+sentance=input("enter the sentance:")
+max_vowal(sentance)
+
